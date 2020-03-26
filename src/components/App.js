30c685fc24324d193e84772767282c831b565522
Paul Dang <paul.dang@bp.com>
2020-03-26 01:08:57 -0500

    Using hooks to store state of searched terms

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/App.css';
 
 import NavBar from './NavBar';
@@ -7,11 +7,43 @@ import FoodCardListContainer from './FoodCards';
 
 
 function App() {
+
+  const initialFoodItems = [
+    { item: 'Jelly Beans' },
+    { item: 'Potato Chip' },
+    { item: 'Used Napkin' }
+  ]
+
+  const [searchedTerms, addSearchTerm] = useState(initialFoodItems);
+
+  const [searchInput, setSearchInputTo] = useState('');
+
+  function handleAddSearchTerm() {
+    addSearchTerm([
+      ...searchedTerms,
+      { item: searchInput }
+    ]);
+
+    setSearchInputTo('');
+  }
+
   return (
     <div className="App">
       <NavBar />
-      <SearchBar />
+      <SearchBar 
+          p_searchInput={searchInput} 
+          p_setSearchInputTo={setSearchInputTo}
+          p_handleAddSearchTerm={handleAddSearchTerm}
+          />
       <FoodCardListContainer />
+      
+      <div style={{marginTop: "50px", padding: "15px"}}>
+        Searched Terms:
+        <div style={{border: "1px solid black", borderRadius: "8px", padding: "10px", margin: "10px"}}>
+          {searchedTerms.map(term => <li>{term.item}</li>)}
+        </div>
+      </div>
+
     </div>
   );
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import '../styles/SearchBar.css';
 
-function SearchBar() {
+const SearchBar = (
+    {
+      p_searchInput, 
+      p_setSearchInputTo,
+      p_handleAddSearchTerm
+    }
+  ) => {
 
+  function keyPressed(event) {
+    if(event.key === "Enter") {
+      p_handleAddSearchTerm();
+    }
+  }
+    
   return (
     <div className="search-bar-container">
       <input 
         type="text"
         id="search-input"
         placeholder="Seach Food Item Here . . . "
+        value={p_searchInput}
+        onChange={e => p_setSearchInputTo(e.target.value)}
+        onKeyPress={keyPressed}
       />
-      <button onClick={searchItem}>
+      <button onClick={p_handleAddSearchTerm}>
         🔍
       </button>
     </div>
   );
 }
 
-function searchItem() {
-  alert("Searching for broccoli ...");
-}
 
 export default SearchBar;
